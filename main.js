b19a0d24a0fd3ca4cff5c8911b9328ca79aa0a77
Mike Stewart <michael.stewart@uibk.ac.at>
2018-04-11 17:27:18 +0200

    Erster Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// alert("Hello World");
+
+let nachricht = "Hallo Welt";
+// alert(nachricht);
+nachricht = "neuer Text";
+// alert(nachricht);
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_variable = "test";
+console.log("Variable: eine_variable: ", eine_variable);
+let highscore = 75.9944;
+console.log("Highscore ist: ", highscore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "test2";
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein
+ganz langer
+text
+und aus`;
+
+console.log(meineNachricht);
+
+let fullName =`Jeffrey "The Dude" Lebwosky`;
+console.log(fullName);
+
+let isOver18 = true;
+console.log("über 18?", isOver18);
+let age = 17 ;
+console.log ("age über 18", age > 18);
